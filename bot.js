dd10da87b1a48c2af8e0f3212efb048c6032c8f7
pbgaming <44928010+pbgaming@users.noreply.github.com>
2019-01-26 14:02:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -322,16 +322,31 @@ client.on("message", async message => {
       }
     });
 
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "invite-center");
+    const logChannel = member.guild.channels.find(channel => channel.name === "epic");
     logChannel.send(${member} Invited by: <@${inviter.id}>);
   });
 });
+
 
 client.on('ready', () => {
    console.log(`----------------`);
